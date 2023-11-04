5211e4308a96b3c6acd4c6a90d9ec1fa34d47f01
MKonon1 <kononenkomyhailo@gmail.com>
2023-11-04 13:07:34 +0000

    projectalmostdone

--- PROJECT/slider.js
+++ PROJECT/slider.js
@@ -1,10 +1,32 @@
-const swiper = new swiper('.swiper', {
+// const swiper = new swiper('.swiper', {
+// 	// Optional parameters
+// 	// loop: true,
+// 	autoplay: {
+// 		delay: 2500,
+// 		// 	  залежить від scroll-bar'у
+// 	},
+
+// 	// If we need pagination
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 	},
+
+// 	// Navigation arrows
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+
+// 	// And if we need scrollbar
+// 	scrollbar: {
+// 		el: '.swiper-scrollbar',
+// 	},
+// });
+
+const swiper = new Swiper('.swiper', {
 	// Optional parameters
-	// loop: true,
-	autoplay: {
-		delay: 2500,
-		// 	  залежить від scroll-bar'у
-	},
+	// direction: 'vertical',
+	loop: true,
 
 	// If we need pagination
 	pagination: {
